5223dc0139114dcb9191d41ae4766fe15e889e9b
johnmogi <anguru@gmail.com>
2019-10-16 17:13:06 +0300

    some mods

--- 21_2/index.js
+++ 21_2/index.js
@@ -1,5 +1,5 @@
 const randArray = [2, 2, 2, 2, 2, 2, 5, 5, 5, 5, 5, 5, 5, 8, 8]
-
+const stage = document.getElementById("stage")
 let major = Math.floor(randArray.length / 2);
 
 function countInArray(arr, e) {
@@ -15,7 +15,10 @@ for (let i = 0; i < randArray.length; i++) {
     // console.log(randArray[i])
     let countAmount = countInArray(randArray, randArray[i])
     if (countAmount >= major) {
-        console.log("the Majority number is : " + randArray[i])
+        stage.append("the given array was : " + randArray)
+        const breaker = document.createElement("br")
+        stage.append(breaker)
+        stage.append("the Majority number is : " + randArray[i])
         break
     }
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# johnmogi.github.io
-# johnmogi.github.io
+```
+<a href="21_1/index.html"> Excercise 1</a>
+            <a href="21_2/index.html"> Excercise 2</a>
+```
