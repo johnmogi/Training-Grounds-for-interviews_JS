3e2e4498ad443194f6a9b736ae6c1fcc3bd8e8f7
johnmogi <anguru@gmail.com>
2019-10-21 08:30:39 +0300

    improving excercise 6

--- 21_6/index.js
+++ 21_6/index.js
@@ -3,7 +3,7 @@ const div = document.createElement("div")
 const div2 = document.createElement("div")
 const main = document.getElementById("main")
 
-var myArray = [1, 1, 1, 2, 3, 8, 8, 8, 9];
+var myArray = [1, 1, 1, 2, 3, 8, 8, 8, 9, 9];
 myArray.sort()
 stage.append(" original array is :" + myArray)
 stage.append(div)
@@ -12,25 +12,36 @@ stage.append(breaker)
 // console.log(myArray.length)
 
 function arrayFix() {
+    let saveLast = myArray[myArray.length - 1]
     for (let i = 0; i < myArray.length; i++) {
+
+        console.log(saveLast)
         if (myArray[0] == myArray[i]) {
             myArray.shift()
         }
+        // if (myArray[i] == myArray[i + 2]) {
+        //     myArray.pop(myArray[i + 2])
+        // }
         if (myArray[i] == myArray[i++]) {
+
             myArray.length--
         }
+
+
         // if (myArray[i] == myArray[i + 2]) {
         //     myArray.length--
         // }
 
     }
+    myArray.push(saveLast)
 }
 arrayFix();
-
 //! this is quite ugly- must fix it in a better way....
 if (myArray[0] == myArray[1]) {
     myArray.shift()
 }
+
+
 // main.innerHTML = ""
 main.append(breaker)
 main.append("output array is :" + myArray)
