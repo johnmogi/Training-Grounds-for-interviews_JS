10aaff805ffbcb8c06e2480f6f950096db4bc61d
johnmogi <anguru@gmail.com>
2019-10-21 19:29:57 +0300

    just fix excercise 1 + bonus a bit

--- EXTRA/index.js
+++ EXTRA/index.js
@@ -12,17 +12,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // pp2[0].addEventListener("click", notifyA)
 var counter = 0;
+var arr = new Array();
 
 function notifyA() {
-
+    arr.push(chooseC.value)
     // const breaker = document.createElement("br")
     counter++
     setTimeout(() => {
         alert(
             "You have chosen the color : " + chooseC.value +
             "\nAmount chosen : " + counter +
-            "\n Previously You have chosen the color : " +
-            "\nAmount of yimes previously chosen : "
+            "\n Previously You have chosen the color : " + arr[arr.length - 1] +
+            "\nAmount of times previously chosen : " + arr.length
         )
     }, 5000);
 
