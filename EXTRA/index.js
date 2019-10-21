74d53ca971ccc12b72b9fb26f0983a4e766081dc
johnmogi <anguru@gmail.com>
2019-10-21 19:20:05 +0300

    no big change

--- EXTRA/index.js
+++ EXTRA/index.js
@@ -1,19 +1,29 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+
+    // window.addEventListener("load", define)
+    // function define() {
     const chooseColor = document.getElementById("chooseC");
     chooseColor.addEventListener("click", notifyA)
+    // }
 });
 
 // const chooseColor = document.getElementById("chooseC");
 // var pp2 = document.getElementsByClassName("choseP")
 
 // pp2[0].addEventListener("click", notifyA)
-
+var counter = 0;
 
 function notifyA() {
-    alert(
-        "You have chosen the color : " + chooseC.value +
-        "Amount chosen : " +
-        "You have chosen the color : " +
-        "Amount chosen : "
-    )
+
+    // const breaker = document.createElement("br")
+    counter++
+    setTimeout(() => {
+        alert(
+            "You have chosen the color : " + chooseC.value +
+            "\nAmount chosen : " + counter +
+            "\n Previously You have chosen the color : " +
+            "\nAmount of yimes previously chosen : "
+        )
+    }, 5000);
+
 }
